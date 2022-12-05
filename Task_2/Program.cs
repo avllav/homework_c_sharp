f65acedfdddce2984682dd359f2245d442286940
alitvinov <alitvinov@outlook.com>
2022-12-05 14:59:17 +0300

    Оптимизирована команда вывода

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -5,20 +5,18 @@ Console.Write("Введите первое число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int b = Convert.ToInt32(Console.ReadLine());
-int max=a;
-if (a>b)
+int max = a;
+if (a > b)
 {
-    max=a;
-    Console.Write ("Из двух введенных чисел максимальное первое введенное число:");
-    Console.WriteLine (a);
+    max = a;
+    Console.Write("Из двух введенных чисел максимальное первое введенное число:" + a);
 }
-if (a<b)
+if (a < b)
 {
-    max=b;
-    Console.Write ("Из двух введенных чисел максимальное второе введенное число:");
-    Console.WriteLine (b);   
+    max = b;
+    Console.Write("Из двух введенных чисел максимальное второе введенное число:" + b);
 }
-if (a == b) 
+else if (a == b)
 {
-    Console.Write ("Введенные числа РАВНЫ");  
+    Console.Write("Введенные числа РАВНЫ");
 }
