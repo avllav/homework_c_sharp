2c9a22a86158a390d35f3362d5ce354be07664df
alitvinov <alitvinov@outlook.com>
2022-12-05 17:40:15 +0300

    Добавлена задача 4 - поиск максимального значения трех введеных чисел

---
+++ Task_4/Program.cs
@@ -0,0 +1,35 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Clear();
+Console.WriteLine("Необходимо ввести три числа");
+Console.Write("Введите первое число: ");
+double a = Convert.ToDouble(Console.ReadLine());
+//-----------------
+Console.Write("Введите второе число: ");
+double b = Convert.ToDouble(Console.ReadLine());
+//-----------------
+Console.Write("Введите третье число: ");
+double c = Convert.ToDouble(Console.ReadLine());
+//Определяем максимальное число из введенных
+double max=a;
+if ((a > b) && (a > c))
+{
+    Console.WriteLine("-----------------------------------------------------");
+    Console.WriteLine("Из введенных чисел максимальное первое число: " + max);
+}
+else if ((b > a) && (b > c))
+{
+    max=b;
+    Console.WriteLine("-----------------------------------------------------");
+    Console.WriteLine("Из введенных чисел максимальное второе число: " + b);
+}
+else if ((c > b) && (c > a))
+{
+    max=c;
+    Console.WriteLine("----------------------------------------------------");
+    Console.WriteLine("Из введенных чисел максимальное третье число: " + c);
+}
+else
+{
+    Console.WriteLine("----------------------------------------------------");
+    Console.WriteLine("Введенные числа РАВНЫ: первое число:" + a + "; второе число:"+b+"; третье число: "+c);
+}
