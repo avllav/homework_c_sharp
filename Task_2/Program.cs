54bb9aa9aec8bb9d92ffa8c54492198a7d9d1e40
alitvinov <alitvinov@outlook.com>
2022-12-05 14:50:47 +0300

    Добавлено проверка на равность чисел

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int max=a;
+if (a>b)
+{
+    max=a;
+    Console.Write ("Из двух введенных чисел максимальное первое введенное число:");
+    Console.WriteLine (a);
+}
+if (a<b)
+{
+    max=b;
+    Console.Write ("Из двух введенных чисел максимальное второе введенное число:");
+    Console.WriteLine (b);   
+}
+if (a == b) 
+{
+    Console.Write ("Введенные числа РАВНЫ");  
+}
