31367133085b4d648e8a9f397fdf9c0d738b1f51
alitvinov <alitvinov@outlook.com>
2022-12-06 14:20:15 +0300

    Оптимизирован код с учетом рекомендаций предователя count++ сделан общим действие + добавлен альтернативный вариант выбора (ветвления через seitch)

--- Task_8/Program.cs
+++ Task_8/Program.cs
@@ -4,6 +4,7 @@ Console.Write("Введите натуральное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int count = 1;
 int evennum;
+/* тож же самый алгоритм при использовании if-else
 if ((num == 1) || (num == 0))
 {
     Console.Write("Четные числа не найдены");
@@ -17,12 +18,27 @@ else
         {
             evennum = count;
             Console.Write(evennum + ", ");
-            count++;
         }
-        else
-        {
-            count++;
-        }
-
+    count++;
     }
+}
+*/
+switch (num) //используем switch 
+{
+    case 1:
+    case 0:
+        Console.Write("Четные числа не найдены");
+        break;
+    default:
+        Console.Write("Четные числа в диапазоне от 1 до " + num + ": ");
+        while (count <= num)
+        {
+            if (count % 2 == 0)
+            {
+                evennum = count;
+                Console.Write(evennum + ", ");
+            }
+            count++;
+        }
+        break;
 }
